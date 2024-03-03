39892f7435e209b288a9fb63d73ed773bb7261da
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-03-03 17:43:55 +0400

    Update flajok2.java

--- flajok2.java
+++ flajok2.java
@@ -1,19 +1,19 @@
 public class flajok2 {
-    public static void main(String[] args) {
-        int count = 0;
+  public static void main(String[] args) {
+    int count = 0;
 
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 1; j <= 4; j++) {
-                for (int k = 1; k <= 4; k++) {
-                    if (i != j && j != k && i != k) {
-                        int num = i * 100 + j * 10 + k;
-                        System.out.println(num);
-                        count++;
-                    }
-                }
-            }
+    for (int i = 1; i <= 4; i++) {
+      for (int j = 1; j <= 4; j++) {
+        for (int k = 1; k <= 4; k++) {
+          if (i != j && j != k && i != k) {
+            int num = i * 100 + j * 10 + k;
+            System.out.println(num);
+            count++;
+          }
         }
-
-        System.out.println("Total number of the three-digit-number is " + count);
+      }
     }
+
+    System.out.println("Total number of the three-digit-number is " + count);
+  }
 }
